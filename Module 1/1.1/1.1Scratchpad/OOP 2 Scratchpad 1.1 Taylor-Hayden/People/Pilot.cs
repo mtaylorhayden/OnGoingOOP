f77f2d80425e556dc277ddf91a818974781c842b
Max <nebraskamax@gmail.com>
2018-12-06 16:09:05 -0600

    Delete

--- Module 1/1.1/1.1Scratchpad/OOP 2 Scratchpad 1.1 Taylor-Hayden/People/Pilot.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace People
-{
-    /// <summary>
-    /// The class which is used to represent a pilot.
-    /// </summary>
-    public class Pilot : Person
-    {
-    }
-}
--- Module 1/1.3/Scratchpad Lab/OOP 2 Scratchpad 1.3 Taylor-Hayden/People/Baggage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace People
-{
-    /// <summary>
-    /// The class which is used to represent a piece of baggage.
-    /// </summary>
-    public class Baggage
-    {
-    }
-}
